7dc500eb49a52e7350696d9f8ce9d38ce4085650
ericatsu <ekaatsu@st.ug.edu.gh>
2024-08-30 14:41:23 +0000

    ✅ Bottom navigation

--- lib/screens/authentication/main_page.dart
+++ lib/screens/authentication/main_page.dart
@@ -1,14 +1,35 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:rickandmorty/widgets/bottom_nav.dart';
+import 'package:rickandmorty/shared/exports.dart';
 
-class MainPage extends StatelessWidget {
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     final user = Get.arguments as Map<String, dynamic>;
 
+    final List<Widget> screens = [
+      CharactersPage(user: user),
+      LocationsPage(),
+      EpisodesPage(),
+    ];
+
     return Scaffold(
-      body: BottomNav(user: user),
+      body: screens[currentIndex],
+      bottomNavigationBar: BottomNav(
+        currentIndex: currentIndex,
+        onTap: (index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
     );
   }
 }
--- lib/widgets/bottom_nav.dart
+++ lib/widgets/bottom_nav.dart
@@ -1,78 +1,71 @@
-import 'package:flutter/cupertino.dart';
-import 'package:rickandmorty/shared/exports.dart';
+import 'package:flutter/material.dart';
 
-class BottomNav extends StatefulWidget {
-  final Map<String, dynamic> user;
+class BottomNav extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
 
-  const BottomNav({super.key, required this.user});
-
-  @override
-  State<BottomNav> createState() => _BottomNavState();
-}
-
-class _BottomNavState extends State<BottomNav> {
-  int _selectedIndex = 0;
-
-  late final List<Widget> _pages;
-
-  @override
-  void initState() {
-    super.initState();
-    _pages = [
-      CharactersPage(user: widget.user),
-      LocationsPage(),
-      EpisodesPage(),
-      //ProfilePage(user: widget.user),
-    ];
-  }
+  const BottomNav({super.key, required this.currentIndex, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
     final double height = MediaQuery.of(context).size.height;
     final double width = MediaQuery.of(context).size.width;
-    return Scaffold(
-      body: _pages[_selectedIndex],
-      backgroundColor: const Color.fromARGB(255, 241, 241, 241),
-      bottomNavigationBar: BottomAppBar(
-        child: Container(
-          height: height * 0.10,
-          decoration: const BoxDecoration(
-            color: Color.fromARGB(255, 230, 230, 230),
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(20),
-              topRight: Radius.circular(20),
-            ),
+
+    return Container(
+      height: height * 0.1,
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 1,
+            blurRadius: 5,
+            offset: const Offset(0, -3),
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  navIcon(CupertinoIcons.group, 0),
-                  navIcon(CupertinoIcons.placemark, 1),
-                  navIcon(CupertinoIcons.tv, 2),
-                  //navIcon(CupertinoIcons.person, 3),
-                ],
-              ),
-            ],
-          ),
-        ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          _buildNavItem(0, 'assets/images/characters.png', 'Characters', width),
+          _buildNavItem(1, 'assets/images/locations.png', 'Locations', width),
+          _buildNavItem(2, 'assets/images/episodes.png', 'Episodes', width),
+        ],
       ),
     );
   }
 
-  Widget navIcon(IconData icon, int index) {
-    return IconButton(
-      icon: Icon(icon, size: 26.0),
-      color: _selectedIndex == index
-          ? const Color.fromARGB(255, 28, 12, 248)
-          : const Color.fromARGB(255, 102, 101, 101),
-      onPressed: () {
-        setState(() {
-          _selectedIndex = index;
-        });
-      },
+  Widget _buildNavItem(
+      int index, String iconPath, String label, double screenWidth) {
+    final isSelected = currentIndex == index;
+
+    return GestureDetector(
+      onTap: () => onTap(index),
+      child: Container(
+        width: screenWidth * 0.3,
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Image.asset(
+              iconPath,
+              width: 32,
+              height: 32,
+              color: isSelected ? Colors.white : const Color.fromARGB(129, 250, 250, 250),
+            ),
+            const SizedBox(height: 4),
+            Text(
+              label,
+              style: TextStyle(
+                color:
+                    isSelected ? Colors.white : const Color.fromARGB(129, 250, 250, 250),
+                fontSize: 13,
+                fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
